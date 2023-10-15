66474bcb49decbf76cd1b2c6a24480eba0c5a08a
DavidShefcik <davidshefcik@gmail.com>
2023-10-15 13:48:14 -0500

    remove red background color

--- frontend/src/pages/Feed.tsx
+++ frontend/src/pages/Feed.tsx
@@ -2,7 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 export default function FeedPage() {
   return (
-    <Box minH="100%" height="1000px" backgroundColor="red.200">
+    <Box minH="100%" height="1000px">
       <Text>Feed</Text>
     </Box>
   );
